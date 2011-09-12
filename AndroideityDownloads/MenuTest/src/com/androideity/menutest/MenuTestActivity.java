2340fc71b35a0bd6d342ff2bb1d4e515d7527905
Aurora Rodriguez <condesa89@gmail.com>
2011-09-11 23:26:49 -0500

    Menu de opciones (estructura)

---
+++ AndroideityDownloads/MenuTest/src/com/androideity/menutest/MenuTestActivity.java
@@ -0,0 +1,23 @@
+package com.androideity.menutest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MenuTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
