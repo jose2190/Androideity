54edac061ed3afdaa04af9390a804a9062503788
Aurora Rodriguez <condesa89@gmail.com>
2011-11-01 10:10:24 -0600

    Broadcast Receivers en Android

---
+++ BroadcastReceiverTest/src/com/androideity/broadcastreceivertest/BroadcastReceiverTestActivity.java
@@ -0,0 +1,25 @@
+package com.androideity.broadcastreceivertest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class BroadcastReceiverTestActivity extends BroadcastReceiver {
+    /** Called when the activity is first created. */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			String state = extras.getString(TelephonyManager.EXTRA_STATE);
+			Log.w("DEBUG--------->", state);
+			if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+				String phoneNumber = extras
+						.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+				Log.w("DEBUG--------->", phoneNumber);
+			}
+		}
+	}
+}
